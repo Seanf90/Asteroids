894edea8b885e3cf8a418b6bbed5665dc902118e
Seanf90 <seanxf@hotmail.co.uk>
2026-01-16 15:09:52 +0000

    created player class and provided movement and rotation for player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 def main():
     pygame.init()
@@ -10,14 +11,20 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen height: {SCREEN_HEIGHT}\nScreen width: {SCREEN_WIDTH}")
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
